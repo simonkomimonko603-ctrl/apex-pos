1f9e34b2cfa8913fa584cef210a122caa47edc6a
simonkomimonko603-ctrl <vas-email@example.com>
2026-08-16 21:53:39 +0200

    Oprava importu v scan.jsx

--- src/scan.jsx
+++ src/scan.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CustomerScanView from './CustomerScanView'; // Tvoj súbor s ponukou
-import { db } from './firebase-config'; // Predpokladám, že tu máš inicializáciu db
+import { db } from "./firebase-config";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
