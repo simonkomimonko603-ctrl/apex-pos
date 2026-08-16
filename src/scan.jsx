11a7dda05ac443a3a78e6ed0c0695f8b7e77a72d
simonkomimonko603-ctrl <vas-email@example.com>
2026-08-16 21:42:34 +0200

    Aktualizovaný main.jsx pre inteligentné prepínanie

---
+++ src/scan.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CustomerScanView from './CustomerScanView'; // Tvoj súbor s ponukou
+import { db } from './firebase-config'; // Predpokladám, že tu máš inicializáciu db
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CustomerScanView db={db} />
+  </React.StrictMode>
+);
