119efd08051320a19c4cf1331fce5215759d785c
simonkomimonko603-ctrl <vas-email@example.com>
2026-08-16 22:01:54 +0200

    Záchrana: Návrat k pôvodnému funkčnému main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CustomerScanView from './CustomerScanView.jsx' // Predpokladám, že tento súbor máš
-import { db } from './firebase.js' // Tu si skontroluj, či sa tvoja firebase volá firebase.js alebo firebase-config.js
 
-const root = createRoot(document.getElementById('root'));
-
-// KĽÚČOVÁ LOGIKA: 
-// Ak adresa obsahuje "/scan", spusti zákaznícky režim, inak spusti pokladňu (App)
-if (window.location.pathname.includes('/scan')) {
-  root.render(
-    <StrictMode>
-      <CustomerScanView db={db} />
-    </StrictMode>
-  );
-} else {
-  root.render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
